59b5e3a9292c2911a90a09927f244171e1fa6b93
Tifeko <106326856+Tifeko@users.noreply.github.com>
2024-06-15 09:25:37 +0200

    add wakatime

--- init.lua
+++ init.lua
@@ -226,6 +226,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   'artempyanykh/marksman',
+  'wakatime/vim-wakatime',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
